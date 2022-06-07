8216c6fe3bbd194b564272811c1b577646936fb4
Alfonso Tienda <afoone@hotmail.com>
2022-06-07 17:21:22 +0200

    types

---
+++ 01-introduccion/dist/tipos_primitivos.js
@@ -0,0 +1,33 @@
+// String
+var c = "Hola";
+console.log(c);
+// Number
+var n;
+n = 12.5;
+n = 12;
+n = -12.789;
+console.log(n);
+//n = 12n
+// Boleano
+var b = false;
+var a;
+a = 4.5;
+a = {
+    a: 1
+};
+a = "kdsfj";
+console.log(a);
+// Arrays
+var nombres;
+nombres = ["pepe", "maria"];
+var edades;
+edades = [1, 32, 43];
+edades.map(function (n) { return n.toFixed(2); });
+// undefined
+var u;
+// null
+var n2;
+console.log(n2);
+n2 = null;
+console.log(n2);
+//edades = ["ks"]
---
+++ 01-introduccion/tipos_primitivos.ts
@@ -0,0 +1,56 @@
+// String
+let c: string = "Hola";
+console.log(c);
+// Number
+let n: number;
+n = 12.5
+n = 12
+n = -12.789
+console.log(n)
+//n = 12n
+
+// Boleano
+let b: boolean = false
+
+let a: any;
+a = 4.5
+a = {
+    a: 1
+}
+a = "kdsfj"
+
+console.log(a)
+
+// Arrays
+let nombres : Array<string>
+nombres = ["pepe", "maria"]
+
+let edades: number[]
+edades = [1,32,43]
+edades.map(
+    n => n.toFixed(2)
+)
+
+// undefined
+let u : undefined;
+
+// null
+let n2: null;
+console.log(n2)
+n2 = null 
+console.log(n2)
+
+
+
+
+//edades = ["ks"]
+
+
+
+
+
+
+
+
+
+
